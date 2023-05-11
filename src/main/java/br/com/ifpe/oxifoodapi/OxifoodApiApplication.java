8173ec632e8bfc4d05b9554dc38c845201d86a90
aluno <aluno@gmail.com>
2023-05-11 15:00:57 -0300

    meu primeiro commit

---
+++ src/main/java/br/com/ifpe/oxifoodapi/OxifoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxifoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxifoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxifoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxifoodapi/OxifoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxifoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxifoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
